f14ff84aa60746c1626a876477d9c05eadca9720
lulets-l <46016065+lulets@users.noreply.github.com>
2019-07-05 22:07:02 +0400

    model changes

--- blogapp/models.py
+++ blogapp/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.conf import settings
+from django.utils import timezone
 
 # Create your models here.
+
+class BlogPost(models.Model):
+	"""
+	Post object for each post
+	"""
+	def __init__(self):
+		title = models.CharField(max_length = 200)
+		author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)		
+		date_created = models.DateTimeField(default = timezone.now)
+		date_published = models.DateTimeField(blank = True, null = True)
+		text = models.TextField()
+
+	def __str__(self):
+		return self.title
+
+	def publish_post(self):
+		self.date_published = timezone.now()
+		self.save()
