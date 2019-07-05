a0bf0b5bf7cc8c082feb1b6573289ad9a226ef90
lulets-l <46016065+lulets@users.noreply.github.com>
2019-07-05 23:57:14 +0400

    model change

--- blogapp/models.py
+++ blogapp/models.py
@@ -8,12 +8,11 @@ class BlogPost(models.Model):
 	"""
 	Post object for each post
 	"""
-	def __init__(self):
-		title = models.CharField(max_length = 200)
-		author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)		
-		date_created = models.DateTimeField(default = timezone.now)
-		date_published = models.DateTimeField(blank = True, null = True)
-		text = models.TextField()
+	title = models.CharField(max_length = 200)
+	author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)		
+	date_created = models.DateTimeField(default = timezone.now)
+	date_published = models.DateTimeField(blank = True, null = True)
+	text = models.TextField()
 
 	def __str__(self):
 		return self.title
