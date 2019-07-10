973371ab68310779626ed387af21db4f1ce9487b
lulets-l <46016065+lulets@users.noreply.github.com>
2019-07-10 21:58:12 +0400

    database change to postgresql

--- mysite/settings.py
+++ mysite/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('DB_NAME',''),
+        'USER': os.environ.get('DB_USER',''),
+        'PASSWORD':os.environ.get('DB_PASS',''),
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
@@ -133,6 +137,6 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
-prod_db = dj_database_url.config(conn_max_age=500)
+prod_db = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(prod_db)
 
