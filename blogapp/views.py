832bf674680348df76ea1729dcffe4328ff24e52
lulets-l <46016065+lulets@users.noreply.github.com>
2019-07-20 23:24:21 +0400

    style css

--- blogapp/views.py
+++ blogapp/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
+from .models import BlogPost
 
 # Create your views here.
 
 def post_list(request):
-	return render(request, 'blogapp/post_list.html', {})
+	posts = BlogPost.objects.filter(date_published__lte=timezone.now()).order_by('date_published')
+	return render(request, 'blogapp/post_list.html', {'posts': posts})
+
+def full_post(request, pk):
+	post = get_object_or_404(BlogPost, pk=pk)
+	return render(request, 'blogapp/full_post.html', {'post': post})
